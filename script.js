e31eca1fff1126627b6c0b2030246c8d8b27b46e
its-swaroopa2025 <swarooparanidevineedi@gmail.com>
2026-03-20 10:14:57 +0000

    Completed half of the  R EST Countries Project

---
+++ script.js
@@ -0,0 +1,24 @@
+fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital')
+.then((res)=>res.json()).then((data)=>{
+    data.forEach(country=>{
+        console.log(country.flags.svg);
+        const countryCard=document.createElement('a');
+countryCard.classList.add('country-card')
+        countryCard.href = `/country.html?name=${country.name.common}`
+
+countryCard.innerHTML=`<img src="${country.flags.svg}" alt="flag">
+    <div class="card-text">
+        <h3 class="card-title">${country.name.common}</h3>
+    <p><b>Population:</b>${country.population.toLocaleString('en-IN')}</p>
+    <p><b>Region:</b>${country.region}</p>
+    <p><b>Capital:</b>${country.capital?country.capital[0]:"no Capital"}</p></div>
+
+</div>`
+
+
+const countriesContainer=document.querySelector('.countries-container')
+
+countriesContainer.append(countryCard)
+    })
+})
+  
